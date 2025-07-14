76d8a0e608d44628bdeedf10d8b3d03be4b6a130
Ace <aceaero671@gmail.com>
2025-07-14 12:01:19 -0400

    Project moved to root and re-initialized

---
+++ AudioVideoConverterTests/AudioVideoConverterTests.swift
@@ -0,0 +1,17 @@
+//
+//  AudioVideoConverterTests.swift
+//  AudioVideoConverterTests
+//
+//  Created by Ayaan Niaz on 7/14/25.
+//
+
+import Testing
+@testable import AudioVideoConverter
+
+struct AudioVideoConverterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
